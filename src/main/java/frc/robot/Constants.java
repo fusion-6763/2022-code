18ad9ea28cead163297a16cf9163b90d71e65969
FUSION 6763 <contact@fusion6763.org>
2022-04-22 18:22:28 -0400

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,9 +20,9 @@ public final class Constants {
         public static final int DRIVE_LEFT_NEO_2 = 4;
         public static final int DRIVE_RIGHT_NEO_1 = 1;
         public static final int DRIVE_RIGHT_NEO_2 = 2;
-        public static final double maxSpeed = 0.8;
-        public static final double maxTurnSpeed = 0.8;
-        public static final double speedWhenIntaking = 0.6;
+        public static final double maxSpeed = 0.4;
+        public static final double maxTurnSpeed = 0.6;
+        public static final double speedWhenIntaking = 0.3;
     }
     public static final class TowerConstants {
         public static final double slowSpeed = 0.5;
