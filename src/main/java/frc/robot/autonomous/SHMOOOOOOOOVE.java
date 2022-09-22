7c800c628a2f1023371a5d65e9f2ce2c035dcd9e
FUSION 6763 <contact@fusion6763.org>
2022-09-22 18:30:15 -0400

    Create SHMOOOOOOOOVE.java

---
+++ src/main/java/frc/robot/autonomous/SHMOOOOOOOOVE.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.autonomous;
+
+import frc.robot.commands.IntakeFast;
+import frc.robot.commands.customDrive;
+import frc.robot.commands.customTower;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Tower;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+/** An example command that uses an example subsystem. */
+public class DumpAndJumpAuto extends SequentialCommandGroup {
+
+  public DumpAndJumpAuto(DriveTrain _driveTrain, Tower _tower) {
+    super(
+      new IntakeFast(_tower).withTimeout(3),
+      new customTower(_tower, 0.0).withTimeout(7),
+      new customDrive(_driveTrain, .5, 0).withTimeout(3)
+    );
+  }
+}
